4ca5e0b541ef34e05d9ae16c14404bbc2cc4af31
王碧瑶 <2487036948@qq.com>
2019-05-27 19:26:19 +0800

    2019_5_27

---
+++ test_2019_5_27_3/test_2019_5_27_3/test.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+int main()
+{
+	//int ret = isdigit('9');//数字字符
+	//printf("ret=%d\n", ret);
+	//printf("%c\n", tolower('A'));//大写转小写
+	printf("%c\n", toupper('a'));//小写转大写
+	system("pause");
+	return 0;
+}
